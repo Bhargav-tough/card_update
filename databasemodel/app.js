d0b2ba68df4d028f9d132fcbd0bbaf3aef9c791d
Bhargav Dubey <bhargav2005dubey@gmail.com>
2026-06-18 00:14:39 +0530

    done

--- databasemodel/app.js
+++ databasemodel/app.js
@@ -15,6 +15,24 @@ app.get('/create', async (req, res) => {
     res.send(create);
 });
 
+app.get("/post/create", async function (req, res) {
+
+    let post = await Post.create({
+        postdata: "hello saare log kaise ho",
+        user: "6a32e69447aff0cd409b0a9c"
+    });
+
+    let user = await User.findOne({
+        _id: "6a32e69447aff0cd409b0a9c"
+    });
+
+    user.posts.push(post._id);
+
+    await user.save();
+
+    res.send({post,user});
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
--- databasemodel/models/user.js
+++ databasemodel/models/user.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/databasemodule');
+
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     age: Number,
-    post: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ]
 });
+
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
